be3b1996430783b0f448d4a14a5d431b7376cd10
Aleksandr-Lee <71881684+Aleksandr-Lee@users.noreply.github.com>
2021-02-08 22:46:51 +0300

    изменение задачи

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
@@ -10,9 +9,9 @@ import "./app.css";
 
 const App = () => {
   const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
+    { label: "Drink ", important: false, id: 1 },
     { label: "Make Awesome App", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 },
+    { label: "Have a lunch", important: false, id: 3 }
   ];
 
   return (
